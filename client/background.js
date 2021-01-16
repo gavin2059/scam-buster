bffb35b6ec879b6f17111e1255412cadb124c779
jsjung00 <justinsoljung@gmail.com>
2021-01-16 11:26:25 -0600

    more comments

--- client/background.js
+++ client/background.js
@@ -4,11 +4,14 @@ const socket = io('http://127.0.0.1:5000');
 socket.on('connect', function() {
  });
 
- //Run when receive the 'scan' event which is triggered when server is alerted in app.py
- // Parameters: url (string)-   
+ // Run when receive the 'scan' event which is triggered when server is alerted in app.py
+ // Parameters: url (string)- url of the detected scam website
+ //             tabID (int) - integer of the tabID that has scam website        
 socket.on('scam', function(url, tabID) {
   // info is a (url, id) tuple
     console.log("server says scam to client", url, tabID);
+    // Gets details about the current tab, once complete if current tab== scam tab will send Message to content.js with scam==True
+    //Q: Race Condition?    
     chrome.tabs.get(tabID, (tab)=> {
       console.log("checking if still on page");
       if(url === tab.url) {
@@ -19,7 +22,7 @@ socket.on('scam', function(url, tabID) {
     }
 );
 
-// Called when a new page is loaded
+// Called when a new page is loaded. 
 chrome.webNavigation.onCompleted.addListener(function(tab) {
 // Gets info from the opened page
   var url = tab.url;
--- client/content.js
+++ client/content.js
@@ -1,10 +1,11 @@
+
 // Listen to message passed and perform action
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log("received");
     if( request.scam === true ) {
       console.log('alerted');
-      alert('take a shower cmon bruh');
+      alert('This page is a phishing site.');
     }
   }
 );
